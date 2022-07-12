ba7bdc5ac92dce6b3da2cb3ca272dc5c0c66491f
aruru2 <104402628+aruru2@users.noreply.github.com>
2022-07-12 10:37:38 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,11 @@
 alert('Hello, tuguru!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-2+3
-'2'+'3'
-Number('2')+Number('3')
-'2'+3
-Number('2')+3
+alert(Math.sqrt(2+3));
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
